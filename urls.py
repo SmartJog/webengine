be783733ae1de925478da636c27291c4ac6b16a5
Thomas Meson <thomas.meson@smartjog.com>
2009-02-09 14:43:59 +0100

    Renamed index into rxtxmanager.

--- urls.py
+++ urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import *
 
 # URLs here will define which RxtxManager plug-in are enable for this set up.
 urlpatterns = patterns('',
-        (r'^$', 'index.views.index', {'output': 'xml'}),
+        (r'^$', 'rxtxmanager.views.index'),
         (r'^telecommande/', include('telecommande.urls')),
         (r'^automations/', include('automations.urls'), {'output': 'json'}),
 )
