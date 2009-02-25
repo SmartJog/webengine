e6592303ede084b6240429a82bcad79f37203f5b
Thomas Meson <thomas.meson@smartjog.com>
2009-02-25 14:26:25 +0100

    Better plugins detection.

--- utils/__init__.py
+++ utils/__init__.py
@@ -5,18 +5,18 @@ def get_valid_plugins():
         Returns a list of valid webengine plugins.
         Returns:    [('name', <module 'webengine.name'>), ...]
     """
-    def __isplugin(dir):
-        """ Nested method of get_valid_plugins, tries to import webengine.<dir>.urls. """
-        # Try to import webengine.<dir>.urls
+    webengine = __import__('webengine')
+    def __isplugin(mod_name):
+        """ Nested method of get_valid_plugins, tries to import webengine.<mod_name>.urls. """
         mod = None
         try:
-            __import__('webengine.' + dir + '.urls', {}, {}, [''])
-            mod = getattr(__import__('webengine'), dir)
+            __import__('webengine.' + mod_name + '.urls', {}, {}, [''])
+            mod = getattr(webengine, mod_name)
         except ImportError:
             return None
-        return dir, mod
+        return mod_name, mod
 
-    wdir = os.getcwd()
+    wdir = webengine.__path__[0]
     # Map os.listdir(wdir) to isplugin, and then "filter" elements that are None
     dirs = [dir for dir in map(__isplugin, [d for d in os.listdir(wdir) if os.path.isdir(os.path.join(wdir, d))]) if dir]
     return dirs
