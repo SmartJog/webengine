08ca8f43ddde947a1f4e3338a2524bd239015e79
Thomas Meson <thomas.meson@smartjog.com>
2009-03-02 15:17:41 +0100

    Changed default index behavior.

--- __init__.py
+++ __init__.py
@@ -1,5 +1,8 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.conf import settings
 
 def index(request):
     """ Default index page. """
+    if settings.DEFAULT_URL != '':
+        return HttpResponseRedirect(settings.DEFAULT_URL)
     return HttpResponse("<h1>It works!</h1>")
