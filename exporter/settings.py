a6f4fea85d7e35c4a653a3f8bee148141d1c5894
Thomas Meson <thomas.meson@smartjog.com>
2009-03-03 15:44:37 +0100

    Removed capability to include global settings 	in per-plugin settings.

--- exporter/settings.py
+++ exporter/settings.py
@@ -1,8 +1,5 @@
 # Settings for Exporter module.
 
-# Import project settings
-from django.conf.settings import *
-
 # Exportable modules.
 # Modules must be a valid python module, in the import path.
 EXPORT_MODULES = ('sys', 'os')
