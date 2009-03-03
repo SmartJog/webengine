7afda2ed62021c46187d88cdcf4d64ec36f1d7f8
Thomas Meson <thomas.meson@smartjog.com>
2009-03-03 15:15:23 +0100

    Renamed utils/generator.py in utils/generators.py

--- utils/generators.py
+++ utils/generators.py
@@ -2,38 +2,52 @@
 
 class Generator(object):
     def __init__(self):
-        pass
+        self._obj = None
+        self._output = 'None'
 
     def generate(self, obj):
         self._obj = obj
 
+    def __str__(self):
+        if self._obj: return self.generate(self._obj)
+        return "<Generator %s>" % str(self._output)
+
 class XmlGenerator(Generator):
     def __init__(self):
         super(XmlGenerator, self).__init__()
+        self._output = 'xml'
 
-    def generate(self):
+    def generate(self, obj):
         super(XmlGenerator, self).generate(obj)
+        #TODO
 
 class SoapGenerator(Generator):
     def __init__(self):
         super(SoapGenerator, self).__init__()
+        self._output = 'soap'
 
     def generate(self, obj):
         super(SoapGenerator, self).generate(obj)
+        #TODO
  
 class JSONGenerator(Generator):
     def __init__(self):
         super(JSONGenerator, self).__init__()
+        self._output = 'json'
 
     def generate(self, obj):
         super(JSONGenerator, self).generate(obj)
+        import django.utils.simplejson as json
+        return json.JSONEncoder().encode(obj)
 
 class DefaultGenerator(Generator):
     def __init__(self):
         super(DefaultGenerator, self).__init__()
+        self._output = 'default'
 
     def generate(self, obj):
         super(DefaultGenerator, self).generate(obj)
+        return str(obj)
 
 class GeneratorFactory(object):
     """ GeneratorFactory handles Generator instances for the right output mode. """
