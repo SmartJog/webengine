941522c3424f595515aca5ed28ea1dddfc358655
Thomas Meson <thomas.meson@smartjog.com>
2009-03-05 18:57:10 +0100

    Importer can get data remotely. 	* Importer can now get data elsewhere that locally. 	(hardcoded to sj-dev-1, will be dynamical later)

--- importer/__init__.py
+++ importer/__init__.py
@@ -6,6 +6,15 @@
 
 import simplejson as json
 
+class ImporterError(Exception):
+    """ Raised when something happen in Importer."""
+    def __init__(self, msg):
+        self.__msg__ = msg
+
+    def __repr__(self):
+        return self.__msg__
+    __str__ = __repr__
+
 # Transparent to module ?
 # Recuperation du module sur /module/get_module, creation de l'objet a partir du retour.
 class Module(object):
@@ -29,16 +38,19 @@ class Module(object):
         return self.__dict__[key]
 
     def __call__(self, *args, **kw):
-        """ This Module instance will act like a method. """
-        # Resolve
-        #FIXME: Will be distant or local, only local by now.
-        req = importer.__request__
-        if req and req.method == 'POST':
-            #Merge POST data with kw
-            data = json.JSONDecoder().decode(req.raw_post_data)
-            #FIXME: json.loads returns unicode strings...
-            d = dict([(str(k), str(v)) for k,v in data.items()])
-            kw.update(d)
+        """
+            This Module instance will act like a method.
+            If method is POST, args are in the raw_post_data,
+            serialized in JSON.
+            POST datas must be formatted like:
+            {'args': ['arg1', 'arg2', 'arg3',..],
+             'kw': {
+                'kw1': 42,
+                'kw2': 'kw2',
+                ...
+             }
+            }
+        """
         # Construct module path.
         curr = self
         path = []
@@ -47,22 +59,53 @@ class Module(object):
             curr = curr.__parent__
         path.append(curr.__name__)
         path.reverse()
-        path = '.'.join(path)
         if self.__islocal__:
-            module, method = path.rsplit('.', 1)
-            m = __import__(module, {}, {}, [''])
-            f = getattr(m, method)
-            # If f is callable, it's a function, otherwise, a module attribute.
-            if callable(f): ret = f(*args, **kw)
-            else: ret = f
-            return ret
+            return self.__local_call__(path, *args, **kw)
         else:
-            return None
+            path = '/'.join(path) + '/'
+            return self.__distant_call__(path, *args, **kw)
 
     def __repr__(self):
         """ Represent instance as a string. """
         return "<%s parent=%s importer=%s>" % (self.__class__.__name__, self.__parent__, self.__importer__)
     __str__ = __repr__
+
+    def __local_call__(self, path, *args, **kw):
+        """ We perform a local call, directly import and call the method. Return as is. """
+        req = importer.__request__
+        if req and req.method == 'POST':
+            #Merge POST data with kw
+            data = json.JSONDecoder().decode(req.raw_post_data)
+            #FIXME: json.loads returns unicode strings...
+            d = dict([(str(k), str(v)) for k,v in data['kw'].items()])
+            kw.update(d)
+            # Cause args is a tuple, create a list before.
+            args = list(args)
+            args += data['args']
+
+        module, method = '.'.join(path[:-1]), path[-1]
+        m = __import__(module, {}, {}, [''])
+        f = getattr(m, method)
+        # If f is callable, it's a function, otherwise, a module attribute.
+        if callable(f): ret = f(*args, **kw)
+        else: ret = f
+        return ret
+
+    def __distant_call__(self, path, *args, **kw):
+        """ Method which performs a distant call to another exporter. """
+        import urllib2
+        # Perform the distant call.
+        data = json.JSONEncoder().encode({'args': args, 'kw': kw})
+        try:
+            req = urllib2.Request(url='https://sj-dev-1.lab/' + path, data=data)
+            f = urllib2.urlopen(req)
+            #TODO: Call the right deserializer.
+            datas = f.read()
+            if datas == '': return None # Nothing to return
+            ret = json.JSONDecoder().decode(datas)
+            return ret
+        except urllib2.HTTPError, e:
+            raise ImporterError("Method %s missing or not allowed." % path)
 
 class Importer(object):
     """ Main class. Contains all modules. """
