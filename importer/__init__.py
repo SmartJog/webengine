d58fe404e48c2bbebe01cc7e31f58f69e3998e02
Thomas Meson <thomas.meson@smartjog.com>
2009-04-01 19:06:42 +0200

    Brand new importer

--- importer/__init__.py
+++ importer/__init__.py
@@ -6,7 +6,7 @@
 
 #import simplejson as json
 #from webengine.utils.log import logger
-#import traceback
+import traceback
 #
 class ImporterError(Exception):
     """ Raised when something happen in Importer."""
@@ -128,6 +128,11 @@ class ImporterError(Exception):
 # New importer BEGINGS HERE
 
 class ImporterBase(object):
+    """
+        Base class for both ImporterModule and ImporterVariable.
+        Provide call/get/set methods, relying on proper self.__objinst__ object.
+        ImporterModule and ImporterVariable are in charge of creating the appropriate object.
+    """
     def __init__(self, conf={}):
         self.__conf__ = conf
         self.__objinst__ = None
@@ -149,7 +154,24 @@ class ImporterBase(object):
 
     def call(self, method, *args, **kw):
         """ Getattr self.__objinst__ which will be a module instance or a variable instance. """
-        return getattr(self.__objinst__, method)(*args, **kw)
+        try:
+            return getattr(self.__objinst__, method)(*args, **kw)
+        except Exception, e:
+            raise ImporterError(str(e), traceback=traceback.format_exc())
+
+    def get(self, attr):
+        """ Getattr self.__objinst__ and returns. """
+        try:
+            return getattr(self.__objinst__, attr)
+        except AttributeError, e:
+            raise ImporterError(str(e), traceback=traceback.format_exc())
+
+    def set(self, attr, value):
+        """ Set value for self.__objinst__.attr. """
+        try:
+            setattr(self.__objinst__, attr, value)
+        except AttributeError, e:
+            raise ImporterError(str(e), traceback=traceback.format_exc())
 
 class ImporterModule(ImporterBase):
     def __init__(self, conf, module):
@@ -157,9 +179,25 @@ class ImporterModule(ImporterBase):
         super(ImporterBase, self).__init__(conf)
         self.__mod__ = module
         try:
+            # Just try to import it
+            __import__(self.__mod__, {}, {}, [''])
+        except ImportError, e:
+            raise ImporterError(str(e), traceback=traceback.format_exc())
+
+    def call(self, method, *args, **kw):
+        """
+            Override ImporterBase.call, but call it after.
+            We need to do this, because if we store the module instance in __objinst__,
+            when saving the session, module is tried to be pickled, and it fails.
+        """
+        try:
             self.__objinst__ = __import__(self.__mod__, {}, {}, [''])
         except ImportError, e:
-            raise ImporterError(str(e))
+            raise ImporterError(str(e), traceback=traceback.format_exc())
+        ret = ImporterBase.call(self, method, *args, **kw)
+        # We need to remove from the object, cause a lot of module are not "pickable"
+        self.__objinst__ = None
+        return ret
 
 class ImporterVariable(ImporterBase):
     def __init__(self, conf, module, klass, *args, **kw):
@@ -167,13 +205,16 @@ class ImporterVariable(ImporterBase):
         self.__mod__ = module
         self.__klass__ = klass
         try:
-            #Instanciate the object
+            #Instantiate the object
             self.__objinst__ = getattr(__import__(self.__mod__, {}, {}, ['']), klass)(*args, **kw)
-        except ImportError, e:
-            raise ImporterError(str(e))
+        except Exception, e:
+            raise ImporterError(str(e), traceback=traceback.format_exc())
 
 class Importer(ImporterBase):
-    """ Main class. Contains all modules. """
+    """
+        Main class. Contains all modules.
+        Call/get/set/instantiate always check if execution must be done remotely.
+    """
     def __init__(self):
         super(Importer, self).__init__()
         self.__scope__ = {}
@@ -182,8 +223,49 @@ class Importer(ImporterBase):
     def call(self, module, method, *args, **kw):
         """
             Perform a call to module.method, passing the given *args, **kw.
+            Override ImporterBase.call()
             Return: module.method return.
         """
+        if 'distant_url' in self.__conf__.keys():
+            mod = '.'.join((module, method))
+            return self.__perform_distant__(mod, 'call', *args, **kw)
+        self.__load_module__(module)
+        return self.__scope__[module].call(method, *args, **kw)
+
+    def get(self, module, attr):
+        """ Retrieve an attr from the given module. """
+        if 'distant_url' in self.__conf__.keys():
+            mod = '.'.join((module, attr))
+            return self.__perform_distant__(mod, 'get')
+        self.__load_module__(module)
+        return self.__scope__[module].get(attr)
+
+    def set(self, module, attr, value):
+        """ Used to set module.attr to value. """
+        if 'distant_url' in self.__conf__.keys():
+            mod = '.'.join((module, attr))
+            return self.__perform_distant__(mod, 'set', value=value)
+        self.__load_module__(module)
+        return self.__scope__[module].set(attr, value)
+
+    def instantiate(self, variable, module, klass, *args, **kw):
+        """
+            Add in the current scope a 'klass' instance from module.
+            Similar to module.klass(*args, **kw).
+            variable will be usable in the scope as others modules.
+        """
+        if 'distant_url' in self.__conf__.keys():
+            mod = '.'.join((module, klass))
+            return self.__perform_distant__(mod, 'instantiate', variable=variable, *args, **kw)
+        if variable in self.__scope__.keys(): return
+        self.__scope__[variable] = ImporterVariable(self.__conf__, module, klass, *args, **kw)
+
+    def bound(self, bound):
+        """ Bound Importer scope to "bound" list. """
+        self.__bound__ = bound
+
+    def __load_module__(self, module):
+        """ Lookup for 'module' in scope, and if not present, create a ImporterModule object. """
         # Module already in scope?
         if module not in self.__scope__.keys():
             first = module.split('.')[0]
@@ -192,33 +274,39 @@ class Importer(ImporterBase):
                 raise ImporterError('Module %s out of bounds' % first)
             # Add module to scope and import
             self.__scope__[module] = ImporterModule(self.__conf__, module)
-        # Call ImporterModule.call() that does the real job
-        return self.__scope__[module].call(method, *args, **kw)
 
-    def get(self, module, attr):
-        """ Retrieve an attr from the given module. """
-        pass
-
-    def set(self, module, attr, value):
-        """ Used to set module.attr to value. """
-        pass
-
-    def instantiate(self, variable, module, klass, *args, **kw):
-        """
-            Add in the current scope a 'klass' instance from module.
-            Similar to module.klass(*args, **kw).
-            variable will be usable in the scope as others modules.
-        """
-        if variable in self.__scope__.keys(): return
-        self.__scope__[variable] = ImporterVariable(self.__conf__, module, klass, *args, **kw)
-
-    def bound(self, bound):
-        """ Bound Importer scope to "bound" list. """
-        self.__bound__ = bound
+    def __perform_distant__(self, module, type, *args, **kw):
+        """ Perform the distant call. """
+        import urllib2, cPickle
+        try:
+            print module, type, args, kw
+            path = module.replace('.', '/') + '/' #Force trailing slash
+            # Should be able to select encoder
+            # TODO: Create a wrapper for cPickle, pickle in fallback
+            data = cPickle.dumps({'type': type, 'args': args, 'kw': kw}, cPickle.HIGHEST_PROTOCOL)
+            req = urllib2.Request(url=self.__conf__['distant_url'] + path, data=data)
+            f = urllib2.urlopen(req)
+            data_read = f.read()
+            if data_read == '': return None
+            data_decoded = cPickle.loads(data_read)
+            return data_decoded
+        except urllib2.HTTPError, e:
+            if e.code == 404:
+                raise ImporterError("Method %s missing." % path, local=False, traceback=traceback.format_exc())
+            elif e.code == 500:
+                data_decoded = cPickle.loads(e.read()) # Read exception
+                raise ImporterError(data_decoded['msg'], local=False, traceback=data_decoded['traceback'])
+        except urllib2.URLError, e:
+            raise ImporterError(str(e), traceback=traceback.format_exc())
+        except cPickle.PickleError, e:
+            raise ImporterError(str(e), traceback=traceback.format_exc())
 
 if __name__ == '__main__':
+    import os
     imp = Importer()
-    imp['distant_uri'] = 'https://sj-dev-1.lab/'
-    print imp.call('os.path', 'basename', '/sdf/xcv/wer/sdf/swww')
-    imp.instantiate('err', 'os', 'error')
-    print imp.call('err', 'listdir', '/path/to/lol')
+    print imp.call('os.path', 'join', 'LOL', 'mdr')
+    imp['distant_url'] = 'https://sj-dev-3.lab/exporter/'
+    imp.instantiate('ctx', 'sjio', 'Ctx')
+    imp.instantiate('file', 'ctx', 'open', '/mnt/space/sjfs/repo/test_importer', os.O_WRONLY | os.O_CREAT | os.O_TRUNC)
+    imp.call('file', 'write', 'IMPORTER IS WORKING.DAMN!')
+    imp.call('file', 'close')
