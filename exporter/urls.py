f20129d72771d202718c878d3874e781840d6baf
Thomas Meson <thomas.meson@smartjog.com>
2009-04-01 15:25:53 +0200

    Totally simplified exporter urls

--- exporter/urls.py
+++ exporter/urls.py
@@ -1,23 +1,3 @@
 from django.conf.urls.defaults import *
-from webengine.exporter import settings
 
-def create_patterns():
-    """
-        Create the patterns from the given exportable modules.
-        Will associate uri like: ^/<module_name>/<method_name>/<query_string>$
-        to the webengine.exporter.dispatch
-    """
-    urls = []
-    # List of exportable modules.
-    mods = settings.EXPORT_MODULES
-    for mod in mods:
-        # Simply check if importable.
-        try:
-            __import__(mod, {}, {}, [''])
-        except ImportError:
-            continue
-        reg = '^%s/(?P<modules>.+)/$' % mod
-        urls.append(url(reg, 'exporter.dispatch', {'base': mod}))
-    return patterns('', *urls)
-
-urlpatterns = create_patterns()
+urlpatterns = patterns('', url('^(?P<base>.+)/(?P<modules>.+)/$', 'exporter.dispatch'))
