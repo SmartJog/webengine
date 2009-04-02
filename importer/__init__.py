a65daf25bb489ce6bf0bf9780c34ea92f9a477d7
Thomas Meson <thomas.meson@smartjog.com>
2009-04-02 19:04:59 +0200

    New version fully working

--- importer/__init__.py
+++ importer/__init__.py
@@ -1,13 +1,9 @@
-"""
-    Intented to be use like this:
+""" Importer """
 
-    importer.(<module>.)+<function>(args)
-"""
+import traceback, os, cookielib, urllib2
 
-#import simplejson as json
-#from webengine.utils.log import logger
-import traceback
-#
+COOKIE_FILE = 'cookies.lwp'
+
 class ImporterError(Exception):
     """ Raised when something happen in Importer."""
     def __init__(self, msg, local=True, traceback=''):
@@ -18,112 +14,6 @@ class ImporterError(Exception):
     def __repr__(self):
         return self.traceback or self.msg or 'Importer failed'
     __str__ = __repr__
-#
-## Transparent to module ?
-## Recuperation du module sur /module/get_module, creation de l'objet a partir du retour.
-#class Module(object):
-#    """
-#        Represent a local/distant python module.
-#        When __getattr__ is called, spawn a new instance of Module() and add
-#        it to the __dict__.
-#        Module instance can also be called like a method, in this case, it
-#        will act like a method. This has been made to allow syntax like
-#        module1.module2.module3...method()
-#    """
-#    def __init__(self, imp, parent, name):
-#        self.__name__ = name
-#        self.__parent__ = parent
-#        self.__importer__ = imp
-#        self.__islocal__ = True
-#
-#    def __getattr__(self, key):
-#        if not (key in self.__dict__):
-#            self.__dict__[key] = Module(self.__importer__, self, key)
-#        return self.__dict__[key]
-#
-#    def __call__(self, *args, **kw):
-#        """
-#            This Module instance will act like a method.
-#            If method is POST, args are in the raw_post_data,
-#            serialized in JSON.
-#            POST datas must be formatted like:
-#            {'args': ['arg1', 'arg2', 'arg3',..],
-#             'kw': {
-#                'kw1': 42,
-#                'kw2': 'kw2',
-#                ...
-#             }
-#            }
-#        """
-#        # Construct module path.
-#        curr = self
-#        path = []
-#        while curr.__parent__ is not None:
-#            path.append(curr.__name__)
-#            curr = curr.__parent__
-#        path.append(curr.__name__)
-#        path.reverse()
-#        if self.__islocal__:
-#            return self.__local_call__(path, *args, **kw)
-#        else:
-#            path = '/'.join(path) + '/'
-#            return self.__distant_call__(path, *args, **kw)
-#
-#    def __repr__(self):
-#        """ Represent instance as a string. """
-#        return "<%s parent=%s importer=%s>" % (self.__class__.__name__, self.__parent__, self.__importer__)
-#    __str__ = __repr__
-#
-#    def __local_call__(self, path, *args, **kw):
-#        """ We perform a local call, directly import and call the method. Return as is. """
-#        req = importer.__request__
-#        if req and req.method == 'POST':
-#            #Merge POST data with kw
-#            data = json.JSONDecoder().decode(req.raw_post_data)
-#            #FIXME: json.loads returns unicode strings...
-#            d = dict([(str(k), str(v)) for k,v in data['kw'].items()])
-#            kw.update(d)
-#            # Cause args is a tuple, create a list before.
-#            args = list(args)
-#            args += data['args']
-#
-#        module, method = '.'.join(path[:-1]), path[-1]
-#        try:
-#            m = __import__(module, {}, {}, [''])
-#            f = getattr(m, method)
-#            # If f is callable, it's a function, otherwise, a module attribute.
-#            if callable(f): ret = f(*args, **kw)
-#            else: ret = f
-#            return ret
-#        except Exception, e:
-#            logger.debug('Importer: Raised: ' + '.'.join(path) + ' - ' + str(e))
-#            raise ImporterError('.'.join(path) + ' - ' + str(e), local=True, traceback=traceback.format_exc()) # Re raise the exception as an ImporterError
-#
-#    def __distant_call__(self, path, *args, **kw):
-#        """
-#            Method which performs a distant call to another exporter.
-#            It calls the distant 'exporter'.
-#            Two possibilities:
-#                - Success, 200 returned, datas returned as JSON. Nothing to do with this, just return after decode.
-#                - Error, raise ImporterError.
-#        """
-#        import urllib2
-#        # Perform the distant call.
-#        try:
-#            data = json.JSONEncoder().encode({'args': args, 'kw': kw})
-#            req = urllib2.Request(url='https://sj-dev-1.lab/' + path, data=data)
-#            f = urllib2.urlopen(req)
-#            #TODO: Call the right deserializer.
-#            datas = f.read()
-#            if datas == '': return None # Nothing to return
-#            data_decoded = json.JSONDecoder().decode(datas)
-#            return data_decoded
-#        except urllib2.HTTPError, e:
-#            if e.code == 404:
-#                raise ImporterError("Method %s missing." % path, local=False, traceback=traceback.format_exc())
-#            elif e.code == 500:
-#                data_decoded = json.JSONDecoder().decode(e.read()) # Read exception
-#                raise ImporterError(data_decoded['msg'], local=False, traceback=data_decoded['traceback'])
 
 # New importer BEGINGS HERE
 
@@ -199,6 +89,14 @@ class ImporterModule(ImporterBase):
         self.__objinst__ = None
         return ret
 
+    def get(self, attr):
+        """
+        """
+        try:
+            return getattr(__import__(self.__mod__, {}, {}, ['']), attr)
+        except ImportError, e:
+            raise ImporterError(str(e), traceback=traceback.format_exc())
+
 class ImporterVariable(ImporterBase):
     def __init__(self, conf, module, klass, *args, **kw):
         super(ImporterBase, self).__init__(conf)
@@ -206,7 +104,10 @@ class ImporterVariable(ImporterBase):
         self.__klass__ = klass
         try:
             #Instantiate the object
-            self.__objinst__ = getattr(__import__(self.__mod__, {}, {}, ['']), klass)(*args, **kw)
+            if isinstance(self.__mod__, ImporterBase):
+                self.__objinst__ = getattr(self.__mod__.__objinst__, klass)(*args, **kw)
+            else:
+                self.__objinst__ = getattr(__import__(self.__mod__, {}, {}, ['']), klass)(*args, **kw)
         except Exception, e:
             raise ImporterError(str(e), traceback=traceback.format_exc())
 
@@ -215,10 +116,18 @@ class Importer(ImporterBase):
         Main class. Contains all modules.
         Call/get/set/instantiate always check if execution must be done remotely.
     """
-    def __init__(self):
+    def __init__(self, file=COOKIE_FILE):
         super(Importer, self).__init__()
         self.__scope__ = {}
         self.__bound__ = None
+        self.__file__ = file
+        self.__cj__ = cookielib.LWPCookieJar(self.__file__)
+        if os.path.isfile(self.__file__):
+            self.__cj__.load(self.__file__)
+        self.__opener__ = urllib2.build_opener(urllib2.HTTPCookieProcessor(self.__cj__))
+
+    def __del__(self):
+        self.__cj__.save()
 
     def call(self, module, method, *args, **kw):
         """
@@ -258,6 +167,8 @@ class Importer(ImporterBase):
             mod = '.'.join((module, klass))
             return self.__perform_distant__(mod, 'instantiate', variable=variable, *args, **kw)
         if variable in self.__scope__.keys(): return
+        if module in self.__scope__.keys():
+            module = self.__scope__[module]
         self.__scope__[variable] = ImporterVariable(self.__conf__, module, klass, *args, **kw)
 
     def bound(self, bound):
@@ -265,7 +176,7 @@ class Importer(ImporterBase):
         self.__bound__ = bound
 
     def __load_module__(self, module):
-        """ Lookup for 'module' in scope, and if not present, create a ImporterModule object. """
+        """ Lookup for 'module' in scope, and if not present, create a ImporterModule object, and return it """
         # Module already in scope?
         if module not in self.__scope__.keys():
             first = module.split('.')[0]
@@ -274,39 +185,27 @@ class Importer(ImporterBase):
                 raise ImporterError('Module %s out of bounds' % first)
             # Add module to scope and import
             self.__scope__[module] = ImporterModule(self.__conf__, module)
+        return self.__scope__[module]
 
     def __perform_distant__(self, module, type, *args, **kw):
         """ Perform the distant call. """
-        import urllib2, cPickle
+        import cPickle
         try:
-            print module, type, args, kw
             path = module.replace('.', '/') + '/' #Force trailing slash
             # Should be able to select encoder
             # TODO: Create a wrapper for cPickle, pickle in fallback
             data = cPickle.dumps({'type': type, 'args': args, 'kw': kw}, cPickle.HIGHEST_PROTOCOL)
             req = urllib2.Request(url=self.__conf__['distant_url'] + path, data=data)
-            f = urllib2.urlopen(req)
+            f = self.__opener__.open(req)
             data_read = f.read()
             if data_read == '': return None
             data_decoded = cPickle.loads(data_read)
             return data_decoded
         except urllib2.HTTPError, e:
-            if e.code == 404:
-                raise ImporterError("Method %s missing." % path, local=False, traceback=traceback.format_exc())
-            elif e.code == 500:
-                data_decoded = cPickle.loads(e.read()) # Read exception
-                raise ImporterError(data_decoded['msg'], local=False, traceback=data_decoded['traceback'])
+            data_decoded = cPickle.loads(e.read()) # Read exception
+            raise ImporterError(data_decoded['msg'], local=False, traceback=data_decoded['traceback'])
         except urllib2.URLError, e:
             raise ImporterError(str(e), traceback=traceback.format_exc())
         except cPickle.PickleError, e:
             raise ImporterError(str(e), traceback=traceback.format_exc())
 
-if __name__ == '__main__':
-    import os
-    imp = Importer()
-    print imp.call('os.path', 'join', 'LOL', 'mdr')
-    imp['distant_url'] = 'https://sj-dev-3.lab/exporter/'
-    imp.instantiate('ctx', 'sjio', 'Ctx')
-    imp.instantiate('file', 'ctx', 'open', '/mnt/space/sjfs/repo/test_importer', os.O_WRONLY | os.O_CREAT | os.O_TRUNC)
-    imp.call('file', 'write', 'IMPORTER IS WORKING.DAMN!')
-    imp.call('file', 'close')
