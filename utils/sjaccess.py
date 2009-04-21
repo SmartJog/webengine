0b752ad6a4c61a718db4b109c8f99a49faa1cf7b
Bastien Abadie <bastien.abadie@smartjog.com>
2009-04-21 16:57:48 +0200

    Use list comparison to sort changelogs versions in reverse order.

--- utils/sjaccess.py
+++ utils/sjaccess.py
@@ -59,16 +59,11 @@ def get_changelogs_versions():
     except:
         return []
 
-    # sort sources version
+    # sort sources version in reverse order
     def cmp_version(x,y):
-        x_list,y_list = x.split('.'),y.split('.')
-        y_list.reverse()
-        for ex in x_list:
-            if len(y_list) == 0: return 1
-            ex, ey = int(ex), int(y_list.pop())
-            if ex == ey: continue
-            return ex > ey and 1 or -1
-        return -1
+        x_list = map(int, x.split('.'))
+        y_list = map(int, y.split('.'))
+        return x_list < y_list and 1 or -1
     all.sort(cmp_version)
 
     return all
