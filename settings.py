a4f7eecbc7d4c36fe43249d40e28fff2ed7482b6
Thomas Meson <thomas.meson@smartjog.com>
2009-04-24 14:25:12 +0200

    Added LocaleMiddleware 	* Allow to select desired language from HTTP header

--- settings.py
+++ settings.py
@@ -58,6 +58,7 @@ TEMPLATE_LOADERS = (
 )
 
 MIDDLEWARE_CLASSES = (
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
