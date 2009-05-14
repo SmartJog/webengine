d76bf92cadbe8b67d25c400ab008b67536cb4df5
Thomas Meson <thomas.meson@smartjog.com>
2009-05-14 16:43:35 +0200

    Changed db table to 'rxtx'

--- settings.py
+++ settings.py
@@ -10,7 +10,7 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASE_ENGINE     = 'postgresql_psycopg2'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME       = 'webengine'             # Or path to database file if using sqlite3.
+DATABASE_NAME       = 'rxtx'             # Or path to database file if using sqlite3.
 DATABASE_USER       = 'webengine'             # Not used with sqlite3.
 DATABASE_PASSWORD   = 'webengine'         # Not used with sqlite3.
 DATABASE_HOST       = ''             # Set to empty string for localhost. Not used with sqlite3.
