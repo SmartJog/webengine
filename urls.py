c1ede731fa1406e6f62f268d3d261f32fdebad11
Nicolas Noirbent <nicolas.noirbent@smartjog.com>
2009-08-31 14:41:56 +0200

    Fix JS i18n, closes: #2306

--- urls.py
+++ urls.py
@@ -4,7 +4,6 @@ from webengine.utils import get_valid_plugins
 # List of patterns to apply, default view is webengine.index
 urlpatterns = patterns('',
     url(r'^$', 'webengine.utils.default_view'),
-    url(r'^jsi18n/(?P<packages>\S+?)/$', 'django.views.i18n.javascript_catalog', name='js_catalog'),
 )
 
 plugs = get_valid_plugins()
@@ -13,6 +12,11 @@ for name, mod in plugs:
     # Append patterns of each plugins
     # Let each plugin define their urlpatterns, just concat them here.
     urlpatterns += patterns('', (r'^' + name + '/', include(name + '.urls')))
+    # JS translations. We have to prepend 'webengine.' to the package
+    # name since it is the way it is spelled in
+    # settings.INSTALLED_APPS; see also #2306.
+    urlpatterns += patterns('', url(r'^jsi18n/' + name + '/$', 'django.views.i18n.javascript_catalog', {'packages': ['webengine.' + name]}))
+
 
 # JUST FOR DEBUG PURPOSE, STATIC PAGES WILL BE SERVED BY APACHE.
 
