99688bbccf92e364bfcde12bb7efe1ab8ddd74ce
Victor Goya <victor.goya@smartjog.com>
2009-10-13 14:01:07 +0200

    Increase log max size from 10k to 20M, closes: #2434

--- utils/log.py
+++ utils/log.py
@@ -6,7 +6,7 @@ from django.conf import settings
 
 logger = logging.getLogger('webengine')
 logger.setLevel(logging.DEBUG)
-handler = logging.handlers.RotatingFileHandler(filename=settings.LOG_FILENAME, maxBytes=10000, backupCount=5)
+handler = logging.handlers.RotatingFileHandler(filename=settings.LOG_FILENAME, maxBytes=20000000, backupCount=5)
 FORMAT = settings.LOG_FORMAT
 formatter = logging.Formatter(FORMAT)
 handler.setFormatter(formatter)
