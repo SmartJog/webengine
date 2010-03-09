50c865bf5c6b390b445e9fb197c449b55556c4fa
Philippe Bridant <philippe.bridant@smartjog.com>
2010-03-09 15:01:15 +0100

    Enabled content_types and persmissions admin

--- utils/admin.py
+++ utils/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from utils.models import *
+from django.contrib import contenttypes
+from django.contrib import auth
 
 class UserSettingAdmin(admin.ModelAdmin):
     pass
 admin.site.register(UserSetting, UserSettingAdmin)
+
+class ContentTypeAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(contenttypes.models.ContentType, ContentTypeAdmin)
+
+class PermissionAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(auth.models.Permission, PermissionAdmin)
